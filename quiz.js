a16e464e97b7e1c5c6c1d0ce9492297d3347c2cf
nisargpawade <70917826+nisargpawade@users.noreply.github.com>
2021-09-26 16:46:48 +0530

    added hypo html and js files

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ const quizForm=document.querySelector(".quiz-form");
 const ques1=document.querySelectorAll(".que1");
 const ques2=document.querySelectorAll(".que2");
 const submitBtn=document.querySelector("#btn-submit");
-const output=document.querySelector("#output");
+const outputQuiz=document.querySelector("#output-quiz");
 
 const correctAnswers=["90°", "right angled"];
 
@@ -16,7 +16,7 @@ function calculateScore(){
         }
         index=index+1
     }
-    output.innerText="Your score is "+score
+    outputQuiz.innerText="Your score is "+score
 }
 
 submitBtn.addEventListener('click', calculateScore);
